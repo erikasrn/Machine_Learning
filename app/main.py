9bf764d60d6ea315e62a0bfb35595ec2cfcadc15
Vovella <84692967+Gumm11@users.noreply.github.com>
2024-11-19 18:48:39 +0700

    Delete app directory

--- app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from app.routes import clustering_router
-
-# Initialize FastAPI app
-app = FastAPI()
-
-# Include the clustering router
-app.include_router(clustering_router)
